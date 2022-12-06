5a0d3eae55b9582c4f915ed8130f09b93172a75e
Bryan Avila <bryanjimavila@gmail.com>
2022-12-06 16:44:52 +0800

    add media queries for responsiveness and dropdown menu for smaller devices

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,6 +3,8 @@ import { Container } from "../layouts/Container"
 import { BtnPrimary, BtnPrimary_Md } from "../elements/Buttons/Button"
 import { H1, H2 } from "../elements/Typography/Headings"
 import NavBar from "../elements/NavBar/NavBar"
+import { SlMenu } from "react-icons/sl";
+
 
 const Header = styled.header`
     min-height: 95vh;
@@ -10,11 +12,6 @@ const Header = styled.header`
     padding: 1rem;
 `
 
-const NavContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-`
 
 const HeroContainer = styled.div`
     margin-top: 100px;
@@ -33,6 +30,73 @@ const HeroButton = styled(BtnPrimary)`
     animation: moveInBottom 2s ease-out 2s;
     animation-fill-mode: backwards;
 `
+const SideMenuContainer = styled.ul`
+    font-size: 1.5rem;
+    visibility: hidden;
+    position: absolute;
+    right: 3%;
+    list-style: none;
+    padding: 1rem 0 1rem 0;
+    background-color: #3F3F46;
+    border-radius: 5px;
+    box-shadow: 0 1rem 2rem rgba(0,0,0,.2);
+    z-index: 999;
+    display: flex;
+    flex-direction: column;
+
+`
+
+const SideMenuLi = styled.li`
+    padding: .5rem 1.5rem .5rem 1.5rem;
+    cursor: pointer;
+    transition: background-color .2s ease-in-out;
+    &:hover  {
+        background-color: #A1A1AA;
+    }
+`
+const SideMenuLink = styled.a`
+
+`
+
+const SideMenuIcon = styled.a`
+    position: relative;
+    transition: color .2s;
+    cursor: pointer;
+
+    &:hover {
+        color: #A1A1AA;
+    }
+`
+
+const SideMenu = styled.div`
+
+    & > ${SideMenuIcon} {
+        display: none;
+    }
+
+    @media (max-width: 640px) { 
+        & > ${BtnPrimary_Md} {
+            display: none;
+        }
+
+        & > ${SideMenuIcon} {
+            display: block;
+        }
+        &:hover ${SideMenuContainer}{
+            visibility: visible;
+            position: absolute;
+        }
+    }
+
+`
+
+const NavContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+
+`
 
 const Hero = () =>{
     return(
@@ -42,9 +106,31 @@ const Hero = () =>{
                     <NavContainer>
                         <H2>Logo</H2>
                         <NavBar />
-                        <BtnPrimary_Md>
-                            Contact
-                        </BtnPrimary_Md>
+                        <SideMenu>
+                            <BtnPrimary_Md>
+                                Contact
+                            </BtnPrimary_Md>
+            
+                            <SideMenuIcon>
+                                <SlMenu size={40} />
+                            </SideMenuIcon>
+
+                            <SideMenuContainer>
+                                <SideMenuLi>
+                                    <SideMenuLink>Home</SideMenuLink>
+                                </SideMenuLi>
+                                <SideMenuLi>
+                                    <SideMenuLink>About</SideMenuLink>
+                                </SideMenuLi>
+                                <SideMenuLi>
+                                    <SideMenuLink>Services</SideMenuLink>
+                                </SideMenuLi>
+                                <SideMenuLi>
+                                    <SideMenuLink>Contact</SideMenuLink>
+                                </SideMenuLi>
+                            </SideMenuContainer>
+                        </SideMenu>
+
                     </NavContainer>
 
                     <HeroContainer>
