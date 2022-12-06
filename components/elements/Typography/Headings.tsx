d9e747d0cc9cdb64ee9f3f9389525fc574b6d41e
Bryan Avila <bryanjimavila@gmail.com>
2022-12-06 16:46:05 +0800

    add media queries for responsiveness

--- components/elements/Typography/Headings.tsx
+++ components/elements/Typography/Headings.tsx
@@ -2,8 +2,20 @@ import styled from "styled-components";
 
 export const H1 = styled.h1`
     font-size: 5rem;
+
+    @media (max-width: 1024px) {
+        font-size: 4rem;
+    }
+
+    @media (max-width: 768px) {
+        font-size: 3rem;
+    }
 `
 export const H2 = styled.h2`
     font-size: 3.25rem;
+
+    @media (max-width: 1024px) {
+        font-size: 2.5rem;
+    }
 `
 export const H3 = styled.h3``
--- components/elements/Typography/Paragraphs.tsx
+++ components/elements/Typography/Paragraphs.tsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const P1 = styled.p`
     font-size: 1.2rem;
     
+    @media (max-width: 1024px) {
+        font-size: 1rem;
+    }
 `
 
 export const P2 = styled(P1)`
